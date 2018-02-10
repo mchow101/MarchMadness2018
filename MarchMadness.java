7e431873eaf49747388f1e8598a90e38934c3cf8
M. Chowdhury <mintychocolate101@gmail.com>
2018-02-10 11:45:10 -0800

    Update MarchMadness.java

--- MarchMadness.java
+++ MarchMadness.java
@@ -1,8 +1,16 @@
 //Mitali Chowdhury
 //REMEMBER TO COMMMENT
+import java.util.ArrayList;
 
 public class MarchMadness {
-	public static void main(String[] args) {
+	static ArrayList<Team> t = new ArrayList<Team>();
+	public MarchMadness() {
+		for(int i = 1; i < 65; i++) 
+			t.add(new Team(i));
+	}
 
+	public static void main(String[] args) {
+		Graphics frame = new Graphics();
+		frame.setTeams(t);
 	}
 }
