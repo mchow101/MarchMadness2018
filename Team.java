ce6309a8d4ff60261b01d3bd5c03c6674eb267bf
M. Chowdhury <mintychocolate101@gmail.com>
2018-02-16 14:59:18 -0800

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,10 +1,16 @@
 public class Team {
 	private int num;
+	private Team opponent;
+	
 	public Team(int num) {
 		this.num = num;
 	}
 	
-	public int getNum() {
-		return num;
-	}
+	public void setOpponent(Team team) { this.opponent = team; }
+	
+	public Team getOpponent() {	return this.opponent; }
+	
+	public String toString() { return "blah" + num; }
+	
+	public int getNum() { return num; }
 }
