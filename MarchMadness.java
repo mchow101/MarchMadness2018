f797041a81621d4ebdd102371ea384ab1481e538
M. Chowdhury <mintychocolate101@gmail.com>
2018-03-10 23:22:48 -0800

    Update MarchMadness.java

--- MarchMadness.java
+++ MarchMadness.java
@@ -1,9 +1,11 @@
-//Mitali Chowdhury
-//REMEMBER TO COMMMENT
+//Mitali Chowdhury :)
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class MarchMadness {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		//initializing graphics class...
+		//this is where the magic starts
 		Graphics frame = new Graphics();
 	}
 }
